8737d8929e03758c85d2b2b75cd006126f580dac
DevAnsar <ansarmirzayi@gmail.com>
2024-02-25 23:15:52 +0330

    Add readme file

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-## React Package Builder
+## Djuno Design
+
+### Introduction
+
+**djuno-design** is a lightweight library for React to develop front-end projects easily.
+
+## Documentation
+
+### 1. Install
+
+npm
+
+    npm i djuno-design
+
+yarn
+
+    yarn add djuno-design
+
+### 2. Import styles to top of your component tree
+
+```jsx
+import 'djuno-design/dist/index.css'
+```
+
+### 3. Simply you can import all components everywere
+
+```jsx
+import { Button } from 'djuno-design'
+
+function App() {
+  return (
+    <div>
+      <Button>Click!</Button>
+    </div>
+  )
+}
+```
+
+## Available components
+
+1. Button
+2. Tooltip
+
+## APIs
+
+### Button
+
+| Name    | Type         | Required | default   | Description |
+| ------- | ------------ | -------- | --------- | ----------- |
+| uiType  | string       | no       | simple    |             |
+| size    | string       | no       | medium    |             |
+| tooltip | TooltipProps | no       | undefined |             |
+
+### Tooltip
+
+| Name    | Type         | Required | default | Description |
+| ------- | ------------ | -------- | ------- | ----------- |
+| content | string       | yes      |         |             |
+| place   | string       | no       | top     |             |
+| theme   | TooltipTheme | no       | primary |             |
+
+## Contributor ✨
+
+[![Contributors](https://contrib.rocks/image?repo=DevAnsar/react-search-hook)](https://github.com/DevAnsar/react-search-hook/graphs/contributors)
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: Size
   tooltip?: TooltipProps
 }
-export type ButtonTypes = 'default' | 'primary' | 'light' | 'icon' | 'danger'
+export type ButtonTypes = 'simple' | 'primary' | 'light' | 'icon' | 'danger'
 
 const Button: React.FunctionComponent<ButtonProps> = ({ children, className, uiType, size, tooltip, ...props }) => {
   const renderButton = () => (
@@ -19,7 +19,7 @@ const Button: React.FunctionComponent<ButtonProps> = ({ children, className, uiT
         'dj-rounded-xl focus:dj-outline-none dj-transition-all disabled:dj-cursor-not-allowed dj-w-max',
         {
           'dj-text-primary-500 dj-bg-transparent hover:dj-bg-primary-50 hover:dj-text-primary-500 hover:dj-border-primary-100 dj-border-2 dj-border-slate-200 dark:dj-bg-secondary-800 dark:hover:dj-bg-secondary-700 dark:dj-text-white dark:dj-border-secondary-700 disabled:dj-border-secondary-400 disabled:hover:dj-bg-transparent disabled:dj-text-secondary-400 disabled:dark:dj-text-secondary-400':
-            uiType === 'default' || uiType === undefined,
+            uiType === 'simple' || uiType === undefined,
           'dj-text-white dj-bg-primary-500 hover:dj-bg-primary-500 hover:dj-shadow-lg hover:dj-shadow-primary-200 dark:hover:dj-shadow-lg dark:hover:dj-shadow-secondary-700 border-2 dj-border-primary-500 focus:dj-ring-2 focus:dj-ring-primary-300 dark:focus:dj-ring-secondary-600 disabled:dj-border-secondary-300 disabled:dj-bg-secondary-300 disabled:dj-text-secondary-400 disabled:dark:dj-bg-secondary-500 disabled:dark:dj-border-secondary-500 disabled:dark:dj-text-secondary-300 disabled:hover:dj-shadow-none':
             uiType === 'primary',
           'dj-text-xs dj-px-3 dj-h-7': size === 'small',
