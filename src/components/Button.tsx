cd0a1c5a7510bf302bdf135a674870b1846bf4b2
DevAnsar <ansarmirzayi@gmail.com>
2024-03-05 01:04:56 +0330

    use cva to make button class variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -23,6 +23,30 @@ import cn from './../utils/cn'
 import Tooltip from './Tooltip'
 import Loading from './Loading'
 import { ButtonProps } from '../types'
+import { cva } from 'class-variance-authority'
+
+const buttonVariants = cva(
+  'dj-rounded-xl focus:dj-outline-none dj-transition-all disabled:dj-cursor-not-allowed dj-w-max dj-flex dj-items-center dj-gap-1 dj-duration-400',
+  {
+    variants: {
+      uiType: {
+        simple:
+          'dj-text-primary-500 dj-bg-transparent hover:dj-bg-primary-50 hover:dj-text-primary-500 hover:dj-border-primary-100 dj-border-2 dj-border-slate-200 dark:dj-bg-secondary-800 dark:hover:dj-bg-secondary-700 dark:dj-text-white dark:dj-border-secondary-700 disabled:dj-border-secondary-300 disabled:hover:dj-bg-transparent disabled:dj-text-secondary-300 disabled:dark:dj-text-secondary-400',
+        primary:
+          'dj-text-white dj-bg-primary-500 hover:dj-bg-primary-500 hover:dj-shadow-lg hover:dj-shadow-primary-200 dark:hover:dj-shadow-lg dark:hover:dj-shadow-secondary-700 border-2 dj-border-primary-500 focus:dj-ring-2 focus:dj-ring-primary-300 dark:focus:dj-ring-secondary-600 disabled:dj-border-secondary-300 disabled:dj-bg-secondary-200 disabled:dj-text-secondary-400 disabled:dark:dj-bg-secondary-500 disabled:dark:dj-border-secondary-500 disabled:dark:dj-text-secondary-300 disabled:hover:dj-shadow-none',
+      },
+      size: {
+        small: 'dj-text-xs dj-px-3 dj-h-7',
+        medium: 'dj-text-sm dj-px-4 dj-h-9',
+        large: 'dj-text-base dj-px-5 dj-h-11',
+      },
+    },
+    defaultVariants: {
+      uiType: 'simple',
+      size: 'medium',
+    },
+  },
+)
 
 /**
  * Button component that allows for customization of UI type, size, loading state, and more.
@@ -66,22 +90,7 @@ const Button: React.FunctionComponent<ButtonProps> = ({
   ...props
 }) => {
   const renderButton = () => (
-    <button
-      {...props}
-      className={cn(
-        'dj-rounded-xl focus:dj-outline-none dj-transition-all disabled:dj-cursor-not-allowed dj-w-max dj-flex dj-items-center dj-gap-1 dj-duration-400',
-        {
-          'dj-text-primary-500 dj-bg-transparent hover:dj-bg-primary-50 hover:dj-text-primary-500 hover:dj-border-primary-100 dj-border-2 dj-border-slate-200 dark:dj-bg-secondary-800 dark:hover:dj-bg-secondary-700 dark:dj-text-white dark:dj-border-secondary-700 disabled:dj-border-secondary-300 disabled:hover:dj-bg-transparent disabled:dj-text-secondary-300 disabled:dark:dj-text-secondary-400':
-            uiType === 'simple' || uiType === undefined,
-          'dj-text-white dj-bg-primary-500 hover:dj-bg-primary-500 hover:dj-shadow-lg hover:dj-shadow-primary-200 dark:hover:dj-shadow-lg dark:hover:dj-shadow-secondary-700 border-2 dj-border-primary-500 focus:dj-ring-2 focus:dj-ring-primary-300 dark:focus:dj-ring-secondary-600 disabled:dj-border-secondary-300 disabled:dj-bg-secondary-200 disabled:dj-text-secondary-400 disabled:dark:dj-bg-secondary-500 disabled:dark:dj-border-secondary-500 disabled:dark:dj-text-secondary-300 disabled:hover:dj-shadow-none':
-            uiType === 'primary',
-          'dj-text-xs dj-px-3 dj-h-7': size === 'small',
-          'dj-text-sm dj-px-4 dj-h-9': size === 'medium' || size === undefined,
-          'dj-text-base dj-px-5 dj-h-11': size === 'large',
-        },
-        className,
-      )}
-    >
+    <button {...props} className={cn(buttonVariants({ uiType, size }), className)}>
       {loading && (
         <Loading
           type={loadingType || 'simple'}
