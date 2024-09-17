29457367356c79d8078fc6994fad3e6503a1d565
DevAnsar <ansarmirzayi@gmail.com>
2024-09-17 14:11:09 +0330

    update to v0.6.4

--- demo/src/Header.tsx
+++ demo/src/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
         <Logo className="w-6" />
         <Text>djuno-design lab🧪</Text>
         <Text uiType="secondary" size="xs">
-          v0.6.2
+          v0.6.4
         </Text>
       </Flex>
       <Flex items="center" className="gap-3">
